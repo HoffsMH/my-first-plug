130d7dfaafa74e1ad53ed55f2802972240221360
HoffsMH <hecker.matthew@gmail.com>
2018-07-15 19:50:25 -0400

    initial commit

---
+++ lib/my_plug.ex
@@ -0,0 +1,18 @@
+defmodule MyPlug do
+  @moduledoc """
+  Documentation for MyPlug.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MyPlug.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
